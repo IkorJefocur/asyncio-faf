8941778c7b0a75564e951707db69964dac13c673
Ikor Jefocur <ikor.jfcr@gmail.com>
2023-06-19 20:39:49 +0300

    Initial commit

---
+++ asyncio_faf/__init__.py
@@ -0,0 +1,18 @@
+from asyncio import create_task, run_coroutine_threadsafe
+
+tasks = set()
+
+def fire_and_forget(coro, loop = None, *args, **kwargs):
+	create = loop.create_task if loop else create_task
+	task = create(coro, *args, **kwargs)
+	tasks.add(task)
+	task.add_done_callback(tasks.discard)
+
+def fire_and_forget_threadsafe(coro, loop, *args, **kwargs):
+	future = run_coroutine_threadsafe(coro, loop, *args, **kwargs)
+	future.add_done_callback(propagate_future_exception)
+
+def propagate_future_exception(future):
+	exc = future.exception()
+	if exc:
+		raise exc
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'Asyncio FAF',
+	packages = find_packages(),
+	version = '0.1',
+	description = 'Fire-and-forget methods for coroutines',
+	author = 'Ikor Jefocur',
+	author_email = 'ikor.jfcr@gmail.com',
+	url = 'https://github.com/IkorJefocur/asyncio-faf'
+)

9677efeb808e759843d02c7acd1e984e66ce2cd8
Ikor Jefocur <ikor.jfcr@gmail.com>
2024-10-12 14:16:42 +0300

    Returning fired task or future

--- asyncio_faf/__init__.py
+++ asyncio_faf/__init__.py
@@ -7,10 +7,12 @@ def fire_and_forget(coro, loop = None, *args, **kwargs):
 	task = create(coro, *args, **kwargs)
 	tasks.add(task)
 	task.add_done_callback(tasks.discard)
+	return task
 
 def fire_and_forget_threadsafe(coro, loop, *args, **kwargs):
 	future = run_coroutine_threadsafe(coro, loop, *args, **kwargs)
 	future.add_done_callback(propagate_future_exception)
+	return future
 
 def propagate_future_exception(future):
 	exc = future.exception()
